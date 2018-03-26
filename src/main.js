b7320a74a395b3ceacc6439c92d4b2ebd884d212
mingdongtu <1518474913@qq.com>
2018-03-26 23:00:39 +0800

    嵌套路由

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import Layout from './components/layout'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import IndexPage from './pages/index'
-
+import DetailPage from './pages/detail'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
 /* eslint-disable no-new */
 Vue.use(VueRouter)
 // VueResource 不需要其他的配置
@@ -17,7 +21,31 @@ let router = new VueRouter({
         {
           path: '/',
           component:IndexPage
-     }
+     },
+   {
+    path:'/detail',
+    component: DetailPage,
+    children:[
+      {
+        path:'analysis',
+        component:DetailAnaPage 
+    },
+    {
+      path:'count',
+      component:DetailCouPage    
+  },
+  
+      {
+          path:'forecast',
+          component:DetailForPage  
+      },
+      {
+        path:'publish',
+        component:DetailPubPage  
+    },
+    ]
+   }
+
        ]
 })
 
---
+++ src/pages/detail/forecast.vue
@@ -0,0 +1,15 @@
+<template>
+     <div>
+          i am forecast
+     </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+ <style>
+ 
+ </style>
+ 
